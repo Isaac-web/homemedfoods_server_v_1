d885d37aadfcf0ef3a47d4eb50f9b3914f276079
Isaac-web <kanytakiy@gmail.com>
2022-10-17 18:39:23 +0000

    Configure database environment variable

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const config = require("config");
 const express = require("express");
 
 const app = express();
 app.get("/ping", (req, res) => {
   res.send("Pong");
 });
+
+console.log(config.get("db"))
 
 require("./startup/middleware")(app);
 require("./startup/routes")(app);
