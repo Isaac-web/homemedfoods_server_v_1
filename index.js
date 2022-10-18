5a4940fec1186398a5336098cb1a5f3c7f02b63a
Isaac-web <kanytakiy@gmail.com>
2022-10-18 07:10:31 +0000

    Testing...

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-const express = require("express");
-const mongoose = require("mongoose");
 const config = require("config");
+const express = require("express");
 
 const app = express();
-require("dot-env");
-
-app.get("/api/ping", (req, res) => {
-  res.send(`Connected to ${config.get("db")}`);
+app.get("/ping", (req, res) => {
+  res.send("Pong");
 });
 
-mongoose
-  .connect(config.get("db"))
-  .then(() => console.log(`Connected to ${process.env.app_db}...`));
-//   .catch((err) => console.log("Could not connect to db..."));
-  
-const port = process.env.PORT || 5000;
-app.listen(5000, () => {console.log(`Listening on port ${port}...`)})
+require("./startup/middleware")(app);
+require("./startup/routes")(app);
+require("./startup/connections")(app);
 
-// const config = require("config");
-// const express = require("express");
-
-// const app = express();
-// app.get("/ping", (req, res) => {
-//   res.send("Pong");
-// });
-
-// console.log(config.get("db"))
-
-// require("./startup/middleware")(app);
-// require("./startup/routes")(app);
-// require("./startup/connections")(app);
-
-// module.exports = app;
-
+module.exports = app;
