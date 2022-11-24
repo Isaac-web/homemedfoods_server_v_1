7dca57944c865d5ee73fbd7793f9c81c17f211c2
Isaac-web <kanytakiy@gmail.com>
2022-11-24 17:00:50 +0000

    Add error handler to branches endpoint

--- routes/branches.js
+++ routes/branches.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const controller = require("../controllers/branches");
 const validateId = require("../middleware/validateId");
+const errorHander = require("../middleware/routeErrorHandler");
 
 const router = express.Router();
 
-router.post("/", controller.createBranch);
-router.get("/", controller.getBranches);
-router.get("/:id", [validateId], controller.getBranch);
-router.patch("/:id", [validateId], controller.updateBranch);
-router.delete("/:id", [validateId], controller.deleteBranch);
+router.post("/", errorHander(controller.createBranch));
+router.get("/", errorHander(controller.getBranches));
+router.get("/:id", [validateId], errorHander(controller.getBranch));
+router.patch("/:id", [validateId], errorHander(controller.updateBranch));
+router.delete("/:id", [validateId], errorHander(controller.deleteBranch));
 
 module.exports = router;
