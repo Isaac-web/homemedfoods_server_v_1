ead39137a9cb2e230310528f57b9732d64e85484
Isaac-web <kanytakiy@gmail.com>
2022-11-28 20:54:09 +0000

    Remove controller cartItems and invitations routes

--- startup/routes.js
+++ startup/routes.js
@@ -6,9 +6,7 @@ const paymentMethods = require("../routes/paymentMethods");
 const products = require("./../routes/products");
 const productCategory = require("./../routes/productCategory");
 const designations = require("./../routes/designations");
-const cartItems = require("../routes/cartItems");
 const branches = require("../routes/branches");
-const invitations = require("./../routes/invitations");
 const users = require("./../routes/users");
 
 module.exports = (app) => {
@@ -18,11 +16,9 @@ module.exports = (app) => {
 
   app.use("/api/addresses", addresses);
   app.use("/api/branches", branches);
-  app.use("/api/cartitems", cartItems);
   app.use("/api/categories", productCategory);
   app.use("/api/designations", designations);
   app.use("/api/discounts", discounts);
-  app.use("/api/invitations", invitations);
   app.use("/api/orders", orders);
   app.use("/api/payment-methods", paymentMethods);
   app.use("/api/products", products);
