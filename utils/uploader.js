e4aee32bb15a6356f95ba7b8363c136916a891de
Isaac-web <kanytakiy@gmail.com>
2022-11-29 12:56:10 +0000

    Clean up

--- utils/uploader.js
+++ utils/uploader.js
@@ -1,12 +1,6 @@
 const cloudinary = require("cloudinary").v2;
 const config = require("config");
 
-// const uploadConfig = {
-//   api_key: "622534129641649",
-//   api_secret: "a-dkeyhieUIr1ZLqDSPHJUe-1x8",
-//   cloud_name: "don6m08ed",
-// };
-
 const uploadConfig = {
   api_key: config.get("cloudinary.api_key"),
   api_secret: config.get("cloudinary.api_secret"),
