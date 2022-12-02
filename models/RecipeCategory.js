af0b18b49c42793821ff6e6070d49de7e7cfafc8
Isaac-web <kanytakiy@gmail.com>
2022-12-02 07:37:01 +0000

    Add RecipeCategory model

---
+++ models/RecipeCategory.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const RecipeCategory = mongoose.model(
+  "RecipeCategory",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    imageUri: {
+      type: String,
+      max: 2048,
+    },
+    desc: {
+      type: String,
+      max: 500,
+    },
+  })
+);
+
+const validate = () => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    imageUri: Joi.string().min(0).max(2048),
+    desc: Joi.string().min(0).max(500),
+  });
+
+  return schema.validate(category);
+};
+
+module.exports.RecipeCategory = RecipeCategory;
+module.exports.validate = validate;
