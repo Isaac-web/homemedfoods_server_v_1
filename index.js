6bf9888046e4aab97b10a47d042ef7a7ab6315dd
Isaac-web <kanytakiy@gmail.com>
2023-01-02 17:16:46 +0000

    Remove Customer Contact

--- index.js
+++ index.js
@@ -26,20 +26,5 @@ require("./startup/routes")(app);
 require("./startup/connections")(httpServer);
 require("./startup/error")(app);
 
-const modifyCustomerContact = async () => {
-  const customers = await Customer.find();
-
-  for (let i = 0; i < customers.length; i++) {
-    const customer = customers[i];
-
-    if (customer.phone.charAt(0) === "0") {
-      customer.phone = "+233" + customer.phone.slice(1);
-      await customer.save();
-    }
-  }
-};
-
-modifyCustomerContact();
-
 module.exports = app;
 
